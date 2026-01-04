0c0a05885453a59d90bc113fc4882399b9d36a37
Baladattu <kbaladattu@gmail.com>
2026-01-04 09:59:20 +0000

    feat: Migrate skeleton and auth pattern components to TypeScript

---
+++ frontend-next/src/components/skeletons/MessageSkeleton.tsx
@@ -0,0 +1,13 @@
+const MessageSkeleton = () => {
+    return (
+        <div className="flex gap-3 items-start">
+            <div className="skeleton w-10 h-10 rounded-full shrink-0"></div>
+            <div className="flex flex-col gap-1">
+                <div className="skeleton h-4 w-40"></div>
+                <div className="skeleton h-4 w-60"></div>
+            </div>
+        </div>
+    );
+};
+
+export default MessageSkeleton;
