a102e414d60fab1efe5d46f379c4dc6a23d922a0
Baladattu <kbaladattu@gmail.com>
2026-01-04 11:00:51 +0000

    fix: Use relative API path to leverage Next.js rewrites

--- frontend-next/src/lib/axios.ts
+++ frontend-next/src/lib/axios.ts
@@ -1,9 +1,7 @@
 import axios from "axios";
 
 const axiosInstance = axios.create({
-    baseURL: process.env.NODE_ENV === "development"
-        ? "http://localhost:5001/api"
-        : "/api",
+    baseURL: "/api",
     withCredentials: true,
 });
 
